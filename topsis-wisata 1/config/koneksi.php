8335e62071bf75c56fe47a8fabe415e682a4b1fd
sulurkopling <abimpamungkass@gmail.com>
2025-06-08 00:29:46 +0700

    Delete topsis-wisata 1 directory

--- topsis-wisata 1/config/koneksi.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-$koneksi = new mysqli("localhost", "root", "123", "db_topsis_wisata_1");
-if ($koneksi->connect_error) {
-    die("Koneksi database gagal: " . $koneksi->connect_error);
-}
-?>
